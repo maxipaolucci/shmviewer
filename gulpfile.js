1d1b5b22970c1e1ca044ba37c1a9a1115bd64730
Maxi Office <maxipaolucci@gmail.com>
2016-01-27 17:42:59 +1300

    gulp initial import

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp'),
+    runSequence = require('run-sequence'),
+    del = require('del'),
+    inject = require('gulp-inject'),
+    angularFileSort = require('gulp-angular-filesort');
+
+gulp.task('default', function(callback) {
+    runSequence('ang-build', callback);
+});
+
+/**
+ * Generates the angular app build. 
+ * First deletes the current build and then generates a new one
+ */
+gulp.task('ang-build', function(callback) {
+    runSequence('ang-clean-build', 'ang-copy-build', 'ang-inject-index', callback);
+});
+
+/**
+ * Deletes the current angular build
+ */
+gulp.task('ang-clean-build', function() {
+    return del(['./app/build'], {force: true});
+});
+
+/**
+ * 
+ */
+gulp.task('ang-inject-index', function() {
+    var tplSrc = ['./app/build/js/libs/*.js', './app/build/js/shmApp.js', './app/build/js/services/*.js', 
+        './app/build/components/**/*.js'];
+    return gulp.src('./app/index.html')
+        .pipe(inject(gulp.src(tplSrc).pipe(angularFileSort()), {ignorePath: 'app/build'}))
+        .pipe(gulp.dest('./app/build'));
+});
+
+
+/**
+ * Copy all the resources for the angular build
+ */
+gulp.task('ang-copy-build', ['ang-copy-components', 'ang-copy-js', 'ang-copy-views', 'ang-copy-css', 'ang-copy-extrafiles']);
+
+/**
+ * Copy angular components into build dir
+ */
+gulp.task('ang-copy-components', function() {
+    return gulp.src('./app/components/**/*.*').pipe(gulp.dest('./app/build/components'));
+});
+
+/**
+ * Copy angular js dir into build js dir
+ */
+gulp.task('ang-copy-js', function() {
+    return gulp.src('./app/js/**/*.js').pipe(gulp.dest('./app/build/js'));
+});
+
+/**
+ * Copy angular views into build views dir
+ */
+gulp.task('ang-copy-views', function() {
+    return gulp.src('./app/views/**/*.html').pipe(gulp.dest('./app/build/views'));
+});
+
+/**
+ * Copy angular css resources and font resources into build css dir
+ */
+gulp.task('ang-copy-css', function() {
+    gulp.src('./app/css/**/*.css').pipe(gulp.dest('./app/build/css'));
+    return gulp.src('./app/css/font-awesome-4.3.0/fonts/*.*').pipe(gulp.dest('./app/build/css/font-awesome-4.3.0/fonts'));
+});
+
+/**
+ * Copy angular specific files into build dir
+ */
+gulp.task('ang-copy-extrafiles', function() {
+    return; //gulp.src('./app/*.html').pipe(gulp.dest('./app/build/'));
+});
